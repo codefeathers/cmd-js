4d5a2522105e5c36278d5763fb4d7c2cb17ebb21
Muthu Kumar <muthukumar@thefeathers.in>
2018-05-25 11:04:16 +0530

    [cli-framework] wip 0.5.0

--- index.js
+++ index.js
@@ -1,37 +1,63 @@
-const { stringMatch, arrayMatch, shouldNext } = require('./utils');
+const fs = require('fs');
 
-class CMD {
-	constructor(argv, options) {
-		this.args = argv;
-		this.options = options;
-		this.noFollow = false;
-	}
+const Promise = require('bluebird');
+const Fuse = require('@codefeathers/fuse');
+const argvParser = require('minimist');
+const requireAll = require('require-all');
 
-	static source (argv) {
-		return argv.slice(2);
-	}
-
-	default (fn, description) {
-		const params = [ this.args.slice(1), this.args.slice(0, 1) ];
-		if(this.noFollow) return this
-		else return fn(...params);
-	}
-
-	use (cmds, fn, description, options) {
-
-		if(this.noFollow) return this;
-		let returns;
-
-		if(stringMatch(this.args, cmds)) { this.noFollow = true; returns = true };
-		if(arrayMatch(this.args, cmds)) { this.noFollow = true; returns = true };
-
-		const next = shouldNext(options, this.options);
-		const params = [ this.args.slice(1), this.args.slice(0, 1) ];
-
-		if(returns && !next) { fn(...params); process.exit(0); }
-		else if (returns) { fn(...params); return this; }
-		else return this;
+const tryCatch = (fn, args) => {
+	try {
+		return fn(args);
+	} catch (e) {
+		return ['Failed', e];
 	}
 }
 
-module.exports = CMD;
+const Errors = [
+	'Not a file or directory path',
+	'Unprocessable entity'
+]
+
+const fileOrDir = path => {
+	let stat = tryCatch(fs.lstatSync, path);
+	if(stat[0] === 'Failed') stat = tryCatch(fs.lstatSync, path + '.js');
+	if(stat[0] === 'Failed') return Errors[0];
+	if(stat.isFile()) return 'file';
+	if(stat.isDirectory()) return 'dir';
+	return Errors[0];
+}
+
+const findActionType = action =>
+	(typeof action === 'object' && !Array.isArray(action))
+		? 'hash'
+		: (typeof action === 'string')
+			? fileOrDir(action)
+			: Errors[1];
+
+class CMDjs {
+	constructor(hash, options) {
+		this.hash = hash;
+		// this.options = options;
+	}
+
+	use(action) {
+		const type = findActionType(action);
+		new Fuse(type)
+			.on(x => Errors.find(y => y === x), _ => { throw new TypeError(action) })
+			.is('hash', _ => { this.hash = { ...this.hash, ...action }})
+			.is('file', _ => this.use(require(action)))
+			.is('dir', _ => {
+				const all = requireAll({ dirname: action, recursive: false });
+				Object.keys(all).forEach(item => this.use(action + '/' + item)); })
+			.resolve();
+		return this;
+	}
+
+	start(proc) {
+		const args = proc.argv.slice(2);
+		this.args = argvParser(args);
+		return this.args;
+	}
+}
+
+module.exports = CMDjs;
